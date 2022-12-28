1bd0e1a14c590aee3aebebad1828c893a974a38f
hugh5 <hughdrummond11@gmail.com>
2022-12-29 06:03:11 +1000

    Initial Commit

---
+++ UQ Social Timetable/UQ_Social_TimetableApp.swift
@@ -0,0 +1,17 @@
+//
+//  UQ_Social_TimetableApp.swift
+//  UQ Social Timetable
+//
+//  Created by Hugh Drummond on 29/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct UQ_Social_TimetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
